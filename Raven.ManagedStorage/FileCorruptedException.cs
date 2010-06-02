14cb1de9252dd8b56d4c92d65c0e0485f03edc34
Ayende Rahien <Ayende@ayende.com>
2010-06-02 16:06:05 +0300

    Exception should be serializable

--- Raven.ManagedStorage/FileCorruptedException.cs
+++ Raven.ManagedStorage/FileCorruptedException.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Raven.ManagedStorage
 {
-    public class FileCorruptedException : Exception
-    {
-        public long LastGoodPosition { get; private set; }
+	[Serializable]
+	public class FileCorruptedException : Exception
+	{
+		public long LastGoodPosition { get; private set; }
 
-        public FileCorruptedException(long lastGoodPosition)
-        {
-            LastGoodPosition = lastGoodPosition;
-        }
-    }
+		public FileCorruptedException(long lastGoodPosition)
+		{
+			LastGoodPosition = lastGoodPosition;
+		}
+		public FileCorruptedException()
+		{
+		}
+
+		public FileCorruptedException(string message) : base(message)
+		{
+		}
+
+		public FileCorruptedException(string message, Exception inner) : base(message, inner)
+		{
+		}
+
+		protected FileCorruptedException(
+			SerializationInfo info,
+			StreamingContext context) : base(info, context)
+		{
+		}
+	}
+   
 }
