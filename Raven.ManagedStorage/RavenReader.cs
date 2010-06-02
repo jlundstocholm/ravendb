1c0bf2420f3fcc83653c062f8978402fc01401e5
Ayende Rahien <Ayende@ayende.com>
2010-06-02 16:25:14 +0300

    Remove unused code

--- Raven.ManagedStorage/RavenReader.cs
+++ Raven.ManagedStorage/RavenReader.cs
@@ -15,13 +15,5 @@ namespace Raven.ManagedStorage
         {
             return Read7BitEncodedInt();
         }
-
-        public int ReadInteger()
-        {
-            var buffer = new byte[4];
-            Read(buffer, 0, 4);
-
-            return BitConverter.ToInt32(buffer, 0);
-        }
     }
 }
